f641d8019f4bfc8c0e57746bb1510074b3010fce
Darleen <darleenfairy33@myyahoo.com>
2024-10-24 17:38:30 -0400

    Delete index-script.js
    Signed-off-by: Darleen <darleenfairy33@myyahoo.com>

--- index-script.js
+++
@@ -1,7 +0,0 @@
-// Copyright 2024 @DarleenFairy33 On github.com
-  // Basic Page Info
-    console.log ('Do not use dev tools on  this website please as dev tools can bt used on my other sites!');
-    console.log ('Please Leave Copyright in my source code');
-    console.log ('Copyright 2024 @DarleenFairy33 On github.com');
-
-// Copyright 2024 @DarleenFairy33 On github.com
